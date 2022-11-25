2713ad589fff851210dc00c798d99fb8a77443f3
univhana <creative0329@dgu.ac.kr>
2022-11-25 12:40:57 +0900

    Chore : src 필요 없는 파일 제거

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+
+const App = () => {
+  return (
+    <div className="App">
+      <nav className="navigation">
+        <a>로고</a>
+        <ul>
+          <a><li>이수과목체크</li></a>
+          <a><li>졸업요건검사</li></a>
+          <a><li>수강과목추천</li></a>
+          <a><li>로그인/회원가입</li></a>
+        </ul>
+      </nav>
+    </div >
+  );
+}
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
