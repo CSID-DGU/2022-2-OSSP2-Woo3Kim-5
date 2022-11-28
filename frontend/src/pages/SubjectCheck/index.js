a021f8ad5526bcdcb8fc6cd3fc9986ba2c3838cc
univhana <creative0329@dgu.ac.kr>
2022-11-28 18:03:10 +0900

    Add : 수강 과목 체크

---
+++ frontend/src/pages/SubjectCheck/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Header from "../../systems/Header";
+
+const SubjectCheck = () => {
+    return(
+        <Header />
+        
+    );
+}
+
+export default SubjectCheck;
