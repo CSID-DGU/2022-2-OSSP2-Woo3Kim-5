12c7c82ec164067e4ad54965d7deb0880c51a05c
naw0n <nwkim@dgu.ac.kr>
2022-12-06 08:36:05 +0900

    [UPDATE] header
    HEADER 내 에 있는 카테고리 HeaderCategory.js로 따로 분류

---
+++ frontend/src/systems/Header/HeaderCategory.js
@@ -0,0 +1,22 @@
+export const HeaderCategory = [
+    {
+        title: "이수과목체크",
+        url: "#",
+        cName: "header-links"
+    },
+    {
+        title: "졸업요건검사",
+        url: "#",
+        cName: "header-links"
+    },
+    {
+        title: "수강과목추천",
+        url: "#",
+        cName: "header-links"
+    },
+    {
+        title: "로그인/회원가입",
+        url: "#",
+        cName: "header-links-mobile"
+    }
+];
--- frontend/src/systems/Header/index.js
+++ frontend/src/systems/Header/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+/*mport React from 'react'
 import { useNavigate } from 'react-router-dom';
 import { Root, LogoImg } from './styled';
 import logo from '../../assets/images/logo.png';
@@ -40,4 +40,41 @@ const Header = () => {
         </Root>
 }
 
-export default Header;
+export default Header;*/
+
+import React, { Component } from "react";
+import { HeaderCategory } from "./HeaderCategory";
+import "./HeaderStyle.css";
+
+class Navbar extends Component {
+    state = { clicked: false };
+    handleClick = () => {
+        this.setState({ clicked: !this.state.clicked });
+    };
+
+    render() {
+        return (
+            <nav className="HeaderCategory">
+                <h1 className="Header-logo">
+                    Woo3Kim <i className="fab fa-logo"></i>
+                </h1>
+                <ul
+                    className={this.state.clicked ? "header-menu active" : "header-menu"}
+                >
+                    {HeaderCategory.map((item, index) => {
+                        return (
+                            <li key={index}>
+                                <a className={item.cName} href={item.url}>
+                                    {item.title}
+                                </a>
+                            </li>
+                        );
+                    })}
+                    <button>로그인/회원가입</button>
+                </ul>
+            </nav>
+        );
+    }
+}
+
+export default Navbar;
