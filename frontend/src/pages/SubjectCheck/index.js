144386ed93e6767ca5b0dbe503aed6790feea1bc
univhana <creative0329@dgu.ac.kr>
2022-12-15 21:38:18 +0900

    Update: SubjectCheck Page

--- frontend/src/pages/SubjectCheck/index.js
+++ frontend/src/pages/SubjectCheck/index.js
@@ -1,41 +1,52 @@
 import React, { useState } from "react";
 import Header from "../../systems/Header";
+import logo from '../../assets/images/subjectguide.png';
+import './index.css';
 
 const SubjectCheck = () => {
-    const [file, setFile] = useState();
+  const [file, setFile] = useState();
 
-    const fileReader = new FileReader();
+  const fileReader = new FileReader();
 
-    const handleOnChange = (e) =>{
-        setFile(e.target.files[0]);
-    };
+  const handleOnChange = (e) => {
+    setFile(e.target.files[0]);
+  };
 
-    const handleOnSubmit = (e) => {
-        e.preventDefault();
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
 
-        if(file){
-            fileReader.onload=function(event){
-                const csvOutput=event.target.result;
-            };
-            fileReader.readasText(file);
-        }
-    };
+    if (file) {
+      fileReader.onload = function (event) {
+        const xlsxOutput = event.target.result;
+      };
+      fileReader.readasText(file);
+    }
+  };
 
-    return(
-        <div className="information">
-        <Header />
-            <form>
-                <input
-                    type={"file"}
-                    id={"csvfileinput"}
-                    accept={".csv"}
-                    onChange={handleOnChange}
-                />
-
-                <button onClick={(e)=>{handleOnSubmit(e);}}>Import CSV</button>
-            </form>
+  return (
+    <div className="information">
+      <Header />
+      <div className="content">
+        <div className="guide">
+          <img src={logo} alt="엑셀가이드"></img>
         </div>
-    );
+        <div>
+          <form>
+            <br/><br/>
+            <input
+              type={"file"}
+              id={"xlsxfileinput"}
+              accept={".xlsx"}
+              onChange={handleOnChange}
+            />
+
+            <button className="submit-btn" onClick={(e) => { handleOnSubmit(e); }}>입력</button>
+          </form>
+        </div>
+
+      </div>
+    </div>
+  );
 }
 
 export default SubjectCheck;
